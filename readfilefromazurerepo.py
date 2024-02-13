7fa47524ea1710ae0ae9a390cb927e2226cb6194
yogeshkumarhcl <155983974+yogeshkumarhcl@users.noreply.github.com>
2024-02-13 10:57:45 +0530

    Update readfilefromazurerepo.py

--- readfilefromazurerepo.py
+++ readfilefromazurerepo.py
@@ -1,11 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 12 23:25:03 2024
+
+@author: v-kumaryo
+"""
+
 import requests
 import base64
-def get_content_azuredevops(pat,project_name,repository_name,file_path,organization_url,branch_name = ''):
 
+
+from flask import Flask,request
+import json
+
+# Initializing flask app
+app = Flask(__name__)
+
+
+
+@app.route('/get_content_azuredevops/<pat>/<project_name>/<repository_name>/<file_name>/<branch_name>', methods=['GET','POST'])
+def get_content_azuredevops(pat,project_name,repository_name,file_name,branch_name = ''):
+
+    organization_url = 'https://dev.azure.com/microsoft'
     authorization = str(base64.b64encode(bytes(':'+pat, 'ascii')), 'ascii')
 
     # The REST API URL for retrieving a file from the Git repository
-    api_url = f"{organization_url}/{project_name}/_apis/git/repositories/{repository_name}/items?path={file_path}&versionDescriptor.version={branch_name}&includeContent=true&api-version=6.0"
+    api_url = f"{organization_url}/{project_name}/_apis/git/repositories/{repository_name}/items?path={file_name}&versionDescriptor.version={branch_name}&includeContent=true&api-version=6.0"
     # Headers for authentication
     headers = {
         'Accept': 'application/json',
@@ -27,3 +46,15 @@ def get_content_azuredevops(pat,project_name,repository_name,file_path,organizat
         print(f"Response: {response.text}")
         print(file_content)
     return response.json().get('content', '')
+
+
+@app.route("/demo", methods=['GET'])
+def demo():
+    return "This is a demo api"
+
+
+
+
+# Running the api
+if __name__ == '__main__':
+    app.run()   
